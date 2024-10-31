76f87a39dba1625fac44c8df82e877dc9c207e94
syedsufiyan888 <syedsufiyan4567@gmail.com>
2024-10-31 19:39:30 +0530

    updated js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var a="Syed";
-var b=10;
+var b=11;
 console.log('Hello world')
 // console.log is function to print values in console
 console.log(typeof a)
@@ -8,4 +8,15 @@ console.log(typeof b)
 // Coditional stmt
 if(b==10){
     console.log('Its a 10 for sure')
+}else{
+    console.log('its not 10 for sure')
+}
+console.log(--b)
+
+// Looping stmts
+
+// 1.for loop
+
+for(var i=1;i<=5;i++){
+    console.log('Hello World')
 }
