9a9af81d78b92eb7c0d2e43e255dc4adca45c898
syedsufiyan888 <syedsufiyan4567@gmail.com>
2024-10-31 19:23:23 +0530

    index.js updated

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log('Hello world')
 // console.log is function to print values in console
 console.log(typeof a)
 console.log(typeof b)
+
+// Coditional stmt
+if(b==10){
+    console.log('Its a 10 for sure')
+}
